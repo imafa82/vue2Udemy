03203cd15ff2ba5eeda1289825248020c18827da
massimiliano.salerno <massimiliano.salerno@liveboxcloud.com>
2017-09-12 14:21:26 +0200

    for

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 new Vue({
     el: 'main',
     data: {
-        text: "Ciao Mondo"
+        text: "Ciao Mondo", 
+        name : "default", 
+        note: 5,
+        films : ["Batman v Superman", "L'allenatore nel Pallone", "Spiderman"],
+        fruits: [
+            {name: "mela", season: "Inverno" , price: 20},
+            {name: "pera", season: "Inverno" , price: 22},
+            {name: "ciliegie", season: "Estate" , price: 30},
+            {name: "fragole", season: "Estate" , price: 30},
+        ]
     }
 });
