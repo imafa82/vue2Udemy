4cd4fbd7cb89b697ef1c9f493866e9c724b8e44b
massimiliano.salerno <massimiliano.salerno@liveboxcloud.com>
2017-09-13 17:11:05 +0200

    delete and create film

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ new Vue({
         text: "Ciao Mondo", 
         name : "default", 
         note: 5,
+        newFilm : null,
+        viewNew: null,
         films : ["Batman v Superman", "L'allenatore nel Pallone", "Spiderman"],
         fruits: [
             {name: "mela", season: "Inverno" , price: 20},
@@ -11,5 +13,16 @@ new Vue({
             {name: "ciliegie", season: "Estate" , price: 30},
             {name: "fragole", season: "Estate" , price: 30},
         ]
+    },
+    methods : {
+        createFilm(){
+            this.films.push(this.newFilm);
+        },
+        deleteFilm(index){   
+            alert(index);
+        },
+        showNewFilm(){
+            this.viewNew = true;
+        }
     }
 });
